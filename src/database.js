f8b7275cc403931cccb988d1d96ea8cb7f504b2b
Cotito27 <65867363+Cotito27@users.noreply.github.com>
2020-09-06 21:49:02 -0500

    version 2

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //mongoose.connect('mongodb://admin:6W3cjggIGqF8yKnl@SG-chat-37819.servers.mongodirector.com:27017/admin', {
-  mongoose.connect('mongodb://admin:6W3cjggIGqF8yKnl@SG-chat-37819.servers.mongodirector.com:27017/admin', {
+//mongoose.connect('mongodb://admin:6W3cjggIGqF8yKnl@SG-chat-37819.servers.mongodirector.com:27017/chat', {
+mongoose.connect('mongodb://Publico:CotitoMononomoxd_27@SG-chat-37819.servers.mongodirector.com:27017/chat', {
     useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
